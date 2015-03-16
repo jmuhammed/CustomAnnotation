abeab467d7d5a8f123ac3f7db9177acafb724504
jmuhammed <jmuhammed@prokarma.com>
2015-03-16 13:30:23 +0530

    Create AnnotationParsing.java

---
+++ AnnotationParsing.java
@@ -0,0 +1,37 @@
+package com.prokarma.annotation;
+ 
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Method;
+
+import com.prokarma.custom.UserStory;
+ 
+public class AnnotationParsing {
+ 
+    public static void main(String[] args) {
+        try {
+            for (Method method : AnnotationParsing.class
+                    .getClassLoader()
+                    .loadClass(("com.prokarma.test.SummaryTestClass"))
+                    .getMethods()) {
+                // checks if MethodInfo annotation is present for the method
+                if (method.isAnnotationPresent(com.prokarma.custom.UserStory.class)) {
+                    try {
+                        // iterates all the annotations available in the method
+                        for (Annotation anno : method.getDeclaredAnnotations()) {
+                            System.out.println("Annotation in Method '"
+                                    + method + "' : " + anno);
+                        }
+                        UserStory userStoryAnnotation = method.getAnnotation(UserStory.class);
+                        System.out.println(userStoryAnnotation.name());
+                       
+                    } catch (Throwable ex) {
+                        ex.printStackTrace();
+                    }
+                }
+            }
+        } catch (SecurityException | ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+ 
+}
