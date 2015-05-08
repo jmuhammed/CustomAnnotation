4c7b7850edd4f52e1b20c4e355c66c979f0c31d1
jmuhammed <jmuhammed@prokarma.com>
2015-05-08 13:07:12 +0530

    Update amexinject.js

--- amexinject.js
+++ amexinject.js
@@ -5,7 +5,7 @@
    var e = '',
         a = contextPath + '/resources/images/markersprite' + (Math.floor(Xa / 10) + 1) + '.png';
         e = 'REC000' !== za[Na].recommID ? 0 === Na && $('.mainWrapper').hasClass('res_Small') && void 0 === ha ? new google.maps.MarkerImage(a, new google.maps.Size(40, 60), new google.maps.Point(40 * Math.floor(Xa % 10), 180), new google.maps.Point(20, 60), new google.maps.Size(400, 240))  : ha - 1 === Xa && $('#suggestionsListContainer').hasClass('userClicked') ? new google.maps.MarkerImage(a, new google.maps.Size(40, 60), new google.maps.Point(40 * Math.floor(Xa % 10), 180), new google.maps.Point(20, 60), new google.maps.Size(400, 240))  : new google.maps.MarkerImage(a, new google.maps.Size(40, 60), new google.maps.Point(40 * Math.floor(Xa % 10), 120), new google.maps.Point(20, 60), new google.maps.Size(400, 240))  : 0 === Na && $('.mainWrapper').hasClass('res_Small') && void 0 === ha ? new google.maps.MarkerImage(a, new google.maps.Size(40, 60), new google.maps.Point(40 * Math.floor(Xa % 10), 60), new google.maps.Point(20, 60), new google.maps.Size(400, 240))  : ha - 1 === Xa && $('#suggestionsListContainer').hasClass('userClicked') ? new google.maps.MarkerImage(a, new google.maps.Size(40, 60), new google.maps.Point(40 * Math.floor(Xa % 10), 60), new google.maps.Point(20, 60), new google.maps.Size(400, 240))  : new google.maps.MarkerImage(a, new google.maps.Size(40, 60), new google.maps.Point(40 * Math.floor(Xa % 10), 0), new google.maps.Point(20, 60), new google.maps.Size(400, 240));
-        var n = new google.maps.Marker({
+        var nMark = new google.maps.Marker({
             position: qa[Na],
             animation: google.maps.Animation.DROP,
             map: ea,
@@ -303,15 +303,15 @@
     }
     function S() {
         
-        n.metadata = {
+        nMark.metadata = {
             type: 'point',
             id: Xa
         },
-        0 === Na && $('.mainWrapper').hasClass('res_Small') ? (Fa = n, Fa.setZIndex(1000))  : void 0 !== ha && ha - 1 === Xa && (Fa = n, Fa.setZIndex(1000)),
+        0 === Na && $('.mainWrapper').hasClass('res_Small') ? (Fa = nMark, Fa.setZIndex(1000))  : void 0 !== ha && ha - 1 === Xa && (Fa = nMark, Fa.setZIndex(1000)),
         Na += 1,
         Xa += 1,
-        Oa.push(n),
-        allMark.push(n),
+        Oa.push(nMark),
+        allMark.push(nMark),
         google.maps.event.addListener(n, 'click', function () {
             if (o('rmaction', 'click_MapPin'), Fa !== this) {
                 var e,
