3b1151cb32759a45075768e77d8191fdc404d654
jmuhammed <jmuhammed@prokarma.com>
2015-05-08 13:03:26 +0530

    Update amexinject.js

--- amexinject.js
+++ amexinject.js
@@ -2,6 +2,18 @@
    var init =  new google.maps.Map(document.getElementById('mapContainer'));
    
    var allMark = [];
+   var e = '',
+        a = contextPath + '/resources/images/markersprite' + (Math.floor(Xa / 10) + 1) + '.png';
+        e = 'REC000' !== za[Na].recommID ? 0 === Na && $('.mainWrapper').hasClass('res_Small') && void 0 === ha ? new google.maps.MarkerImage(a, new google.maps.Size(40, 60), new google.maps.Point(40 * Math.floor(Xa % 10), 180), new google.maps.Point(20, 60), new google.maps.Size(400, 240))  : ha - 1 === Xa && $('#suggestionsListContainer').hasClass('userClicked') ? new google.maps.MarkerImage(a, new google.maps.Size(40, 60), new google.maps.Point(40 * Math.floor(Xa % 10), 180), new google.maps.Point(20, 60), new google.maps.Size(400, 240))  : new google.maps.MarkerImage(a, new google.maps.Size(40, 60), new google.maps.Point(40 * Math.floor(Xa % 10), 120), new google.maps.Point(20, 60), new google.maps.Size(400, 240))  : 0 === Na && $('.mainWrapper').hasClass('res_Small') && void 0 === ha ? new google.maps.MarkerImage(a, new google.maps.Size(40, 60), new google.maps.Point(40 * Math.floor(Xa % 10), 60), new google.maps.Point(20, 60), new google.maps.Size(400, 240))  : ha - 1 === Xa && $('#suggestionsListContainer').hasClass('userClicked') ? new google.maps.MarkerImage(a, new google.maps.Size(40, 60), new google.maps.Point(40 * Math.floor(Xa % 10), 60), new google.maps.Point(20, 60), new google.maps.Size(400, 240))  : new google.maps.MarkerImage(a, new google.maps.Size(40, 60), new google.maps.Point(40 * Math.floor(Xa % 10), 0), new google.maps.Point(20, 60), new google.maps.Size(400, 240));
+        var n = new google.maps.Marker({
+            position: qa[Na],
+            animation: google.maps.Animation.DROP,
+            map: ea,
+            title: za[Na].merchantName,
+            icon: e,
+            zIndex: 100,
+            optimized: false
+        });
 
     function e(e) {
         $('.mainWrapper').removeClass('res_Large res_Medium res_Small'),
@@ -290,18 +302,7 @@
         }
     }
     function S() {
-        var e = '',
-        a = contextPath + '/resources/images/markersprite' + (Math.floor(Xa / 10) + 1) + '.png';
-        e = 'REC000' !== za[Na].recommID ? 0 === Na && $('.mainWrapper').hasClass('res_Small') && void 0 === ha ? new google.maps.MarkerImage(a, new google.maps.Size(40, 60), new google.maps.Point(40 * Math.floor(Xa % 10), 180), new google.maps.Point(20, 60), new google.maps.Size(400, 240))  : ha - 1 === Xa && $('#suggestionsListContainer').hasClass('userClicked') ? new google.maps.MarkerImage(a, new google.maps.Size(40, 60), new google.maps.Point(40 * Math.floor(Xa % 10), 180), new google.maps.Point(20, 60), new google.maps.Size(400, 240))  : new google.maps.MarkerImage(a, new google.maps.Size(40, 60), new google.maps.Point(40 * Math.floor(Xa % 10), 120), new google.maps.Point(20, 60), new google.maps.Size(400, 240))  : 0 === Na && $('.mainWrapper').hasClass('res_Small') && void 0 === ha ? new google.maps.MarkerImage(a, new google.maps.Size(40, 60), new google.maps.Point(40 * Math.floor(Xa % 10), 60), new google.maps.Point(20, 60), new google.maps.Size(400, 240))  : ha - 1 === Xa && $('#suggestionsListContainer').hasClass('userClicked') ? new google.maps.MarkerImage(a, new google.maps.Size(40, 60), new google.maps.Point(40 * Math.floor(Xa % 10), 60), new google.maps.Point(20, 60), new google.maps.Size(400, 240))  : new google.maps.MarkerImage(a, new google.maps.Size(40, 60), new google.maps.Point(40 * Math.floor(Xa % 10), 0), new google.maps.Point(20, 60), new google.maps.Size(400, 240));
-        var n = new google.maps.Marker({
-            position: qa[Na],
-            animation: google.maps.Animation.DROP,
-            map: ea,
-            title: za[Na].merchantName,
-            icon: e,
-            zIndex: 100,
-            optimized: false
-        });
+        
         n.metadata = {
             type: 'point',
             id: Xa
